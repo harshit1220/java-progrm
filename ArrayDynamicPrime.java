bc1ebc8bfb6f6af7e97b4ab908ee2c792f78db84
Harshit jaiswal <harshitjaiswal2005@gmail.com>
2024-01-05 18:04:47 +0530

    initial change

--- ArrayDynamicPrime.java
+++ ArrayDynamicPrime.java
@@ -3,9 +3,35 @@ class ArrayDynamicPrime
 {
 	public static void main(String[]args)
 	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number to find the number of prime: ");
-		int n=sc.nextInt();
-		
+		int no=10;
+		int []primeNumber=primeNumber(no);
+		System.out.println(Arrays.toString((primeNumber)));
+	}
+	
+	public static int[] primeNumber(int no)
+	{
+		int []primeArray=new int[no];
+		int cnt=0;
+		for(int i=3;true;i++)
+		{
+			int count=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+					break;
+				}
+			}
+			if(count==0)
+			{
+				primeArray[cnt++]=i;
+				if(cnt==no)
+				{
+					break;
+				}
+			}
+		}
+		return primeArray;
 	}
 }
